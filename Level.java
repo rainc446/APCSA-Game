ea1eb31dd6a9c6e9b9b867f2afc6735148ad82e3
Rain Chen <rainc8@nycstudents.net>
2025-03-18 12:18:02 -0400

    finished level class and methods

---
+++ Level.java
@@ -0,0 +1,26 @@
+public class Level
+{
+    private boolean goalState;
+    private int pointValue;
+
+    public void addPoints (int points) {
+        pointValue += points;
+    }
+
+    public void setGoalState (boolean goalState) {
+        this.goalState = goalState;
+    }
+
+    /** Returns true if the player reached the goal on this level and returns false otherwise */
+    public boolean goalReached()
+    {
+        /* implementation not shown */
+        return goalState;
+    }
+    /** Returns the number of points (a positive integer) recorded for this level */
+    public int getPoints()
+    { /* implementation not shown */
+        return pointValue;
+    }
+// There may be instance variables, constructors, and methods that are not shown.
+}
