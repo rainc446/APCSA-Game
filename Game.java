43ea4783c79308fdeb802b13a980e87a1b0732b0
Rain Chen <rainc8@nycstudents.net>
2025-03-18 12:34:19 -0400

    es

--- Game.java
+++ Game.java
@@ -3,16 +3,31 @@ public class Game
     private Level levelOne;
     private Level levelTwo;
     private Level levelThree;
+
+    private Level[] levels = {levelOne, levelTwo, levelThree};
+
+    private boolean bonusGame;
     /** Postcondition: All instance variables have been initialized. */
-    public Game()
-    { /* implementation not shown */ }
-    /** Returns true if this game is a bonus game and returns false otherwise */  public boolean isBonus()
-    { /* implementation not shown */ }
+    public Game(int[] scores, boolean[] goals)
+    {
+        for (levels: Level level) {
+
+        }
+
+        /* implementation not shown */
+    }
+    /** Returns true if this game is a bonus game and returns false otherwise */
+    public boolean isBonus()
+    {
+        /* implementation not shown */
+        return bonusGame;
+    }
     /** Simulates the play of this Game (consisting of three levels) and updates all relevant * game data
      */
     public void play()
     { /* implementation not shown */ }
-    /** Returns the score earned in the most recently played game, as described in part (a) */  public int getScore()
+    /** Returns the score earned in the most recently played game, as described in part (a) */
+    public int getScore()
     { /* to be implemented in part (a) */ }
     /** Simulates the play of num games and returns the highest score earned, as
      * described in part (b)
@@ -20,5 +35,6 @@ public class Game
      */
     public int playManyTimes(int num)
     { /* to be implemented in part (b) */ }
+
 // There may be instance variables, constructors, and methods that are not shown.
 }
